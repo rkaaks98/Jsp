41507ae1ad1c1db4b9638183d4232cdb318738e9
wnstj050505 <wnstj050505@gmail.com>
2025-02-25 17:51:23 +0900

    ch10

--- ch10/src/main/java/dao/User3DAO.java
+++ ch10/src/main/java/dao/User3DAO.java
@@ -33,19 +33,49 @@ public class User3DAO extends DBHelper {
     }
   }
   public User3DTO selectUser3(String uid) {
-    
+    User3DTO dto = null;
     try {
+    	
+    	conn = getConnection();
+    	psmt = conn.prepareStatement(SQL.SELECT_USER3);
+    	psmt.setString(1, uid);
+    	
+    	rs = psmt.executeQuery();
+    	
+    	if(rs.next()) {
+    		dto = new User3DTO();
+    		dto.setUid(rs.getString(1));
+    		dto.setName(rs.getString(2));
+    		dto.setBirth(rs.getString(3));
+    		dto.setHp(rs.getString(4));
+    		dto.setAddr(rs.getString(5));
+    	}
+    	closeAll();
       
     } catch (Exception e) {
       e.printStackTrace();
     }
-    return null;
+    return dto;
   }
   public List<User3DTO> selectAllUser3() {
+	  
     List<User3DTO> dtos = new ArrayList<User3DTO>();
     
     try {
-      
+    	conn = getConnection();
+    	stmt = conn.createStatement();
+    	rs = stmt.executeQuery(SQL.SELECT_ALL_USER3);
+    	
+    	while(rs.next()) {
+    		User3DTO dto = new User3DTO();
+    		dto.setUid(rs.getString(1));
+    		dto.setName(rs.getString(2));
+    		dto.setBirth(rs.getString(3));
+    		dto.setHp(rs.getString(4));
+    		dto.setAddr(rs.getString(5));
+    		dtos.add(dto);
+    	}
+      closeAll();
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -54,6 +84,15 @@ public class User3DAO extends DBHelper {
   public void updateUser3(User3DTO dto) {
     
     try {
+    	conn = getConnection();
+    	psmt = conn.prepareStatement(SQL.UPDATE_USER3);
+    	psmt.setString(1, dto.getName());
+    	psmt.setString(2, dto.getBirth());
+    	psmt.setString(3, dto.getHp());
+    	psmt.setString(4, dto.getAddr());
+    	psmt.setString(5, dto.getUid());
+    	psmt.executeUpdate();
+    	closeAll();
       
     } catch (Exception e) {
       e.printStackTrace();
@@ -63,6 +102,11 @@ public class User3DAO extends DBHelper {
   public void deleteUser3(String uid) {
     
     try {
+    	conn = getConnection();
+    	psmt = conn.prepareStatement(SQL.DELETE_USER3);
+    	psmt.setString(1, uid);
+    	psmt.executeUpdate();
+    	closeAll();
       
     } catch (Exception e) {
       e.printStackTrace();
